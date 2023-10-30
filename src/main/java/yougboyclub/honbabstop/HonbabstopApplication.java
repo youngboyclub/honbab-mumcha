bdd0d14b02c5b0b7a819d94e919f1870a1390149
rowoon <dev.rowoon@gmail.com>
2023-10-30 21:40:25 +0900

    init: 환경설정 후 1차 푸쉬

---
+++ src/main/java/yougboyclub/honbabstop/HonbabstopApplication.java
@@ -0,0 +1,13 @@
+package yougboyclub.honbabstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonbabstopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonbabstopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yougboyclub/honbabstop/HonbabstopApplicationTests.java
@@ -0,0 +1,13 @@
+package yougboyclub.honbabstop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonbabstopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
