ea55c7e1ab17722598ad1e8edc1cd143c71541ba
rowoon <dev.rowoon@gmail.com>
2023-11-01 01:36:03 +0900

    init: 라우터 설정 완료 #1

--- src/main/frontend/src/app/App.js
+++ src/main/frontend/src/app/App.js
@@ -1,11 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "pages/MainPage";
+import LoginPage from "pages/LoginPage";
+import RegisterPage from "pages/RegisterPage";
+import BoardPage from "pages/BoardPage";
+import BoardDetailPage from "pages/BoardDetailPage";
+import WritePage from "pages/WritePage";
+import MyPagePage from "pages/MyPagePage";
+
+
+
 
 
 function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+//                <Route path="/" element={<MainPage />} />
+//                <Route path="/login" element={<LoginPage />} />
+//                <Route path="/register" element={<RegisterPage />} />
+//                <Route path="/boardList" element={<BoardPage />} />
+//                <Route path="/boardDetail" element={<BoardDetailPage />} />
+//                <Route path="/write" element={<WritePage />} />
+//                <Route path="/my" element={<MyPagePage />} />
+            </Routes>
+        </BrowserRouter>
     );
 
 
