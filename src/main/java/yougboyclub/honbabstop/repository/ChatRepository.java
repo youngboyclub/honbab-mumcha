bce536f104fa965b03f8b1c2829abbea75e0b776
rowoon <dev.rowoon@gmail.com>
2023-11-03 14:30:07 +0900

    chore: 몽고디비 연결 채팅 레포지토리 생성 #26

---
+++ src/main/java/yougboyclub/honbabstop/repository/ChatRepository.java
@@ -0,0 +1,13 @@
+package yougboyclub.honbabstop.repository;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import yougboyclub.honbabstop.domain.Chat;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+
+    //Flux : 흐름 => 데이터를 계속 흘려서 받겠다(한번만 받고 끝나는게 아니라!)
+    @Query("{sender: ?0,receiver: ?1}") // 몽고디비 문법임 각각 sender와 receiver의 내용을 받아오겠다
+    Flux<Chat> mFindBySender(String sender, String receiver);
+}
