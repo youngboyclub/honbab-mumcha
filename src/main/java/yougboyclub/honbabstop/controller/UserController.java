8f1a9c8b692152feb26bc024a592dcb7ed268d13
thkim610 <thkim610@naver.com>
2023-11-03 18:09:17 +0900

    feat: UserController 파일 생성 #30

---
+++ src/main/java/yougboyclub/honbabstop/controller/UserController.java
@@ -0,0 +1,20 @@
+package yougboyclub.honbabstop.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import yougboyclub.honbabstop.dto.requestUserDto;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    //회원가입
+    @PostMapping("/new")
+    public ResponseEntity<String> join(@RequestBody requestUserDto dto){
+        System.out.println("dto = " + dto);
+        return ResponseEntity.ok().body("회원가입이 성공하였습니다.");
+    }
+}
