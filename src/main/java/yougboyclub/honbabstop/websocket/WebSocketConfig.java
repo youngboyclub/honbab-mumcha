464da8ee1ce73fa841ec4d52e7da367583fbf52f
rowoon <dev.rowoon@gmail.com>
2023-11-03 10:43:40 +0900

    feat: 소켓 이용한 채팅기능 구현중  #26

--- src/main/java/yougboyclub/honbabstop/websocket/WebSocketConfig.java
+++ src/main/java/yougboyclub/honbabstop/websocket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package youngboyclub.websocket;
+package yougboyclub.honbabstop.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
