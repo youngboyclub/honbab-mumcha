da210a608ab47934de08ec35a23383fd1853a41a
rowoon <dev.rowoon@gmail.com>
2023-11-03 14:29:44 +0900

    chore: 채팅 엔티티 생성 #26

---
+++ src/main/java/yougboyclub/honbabstop/domain/Chat.java
@@ -0,0 +1,19 @@
+package yougboyclub.honbabstop.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "chat")
+public class Chat {
+    @Id
+    private String id;
+    private String message;
+    private String sender; // 보내는 사람
+    private String receiver; // 받는 사람
+
+    private LocalDateTime createdAt;
+}
