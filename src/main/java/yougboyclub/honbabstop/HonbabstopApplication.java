31083f4baa9f0c6589a2b916d21fa9b1b9bda672
thkim610 <thkim610@naver.com>
2023-11-05 12:47:31 +0900

    mod: 등록일 자동 생성을 위한 관련 Anotation 추가 #30

--- src/main/java/yougboyclub/honbabstop/HonbabstopApplication.java
+++ src/main/java/yougboyclub/honbabstop/HonbabstopApplication.java
@@ -2,8 +2,10 @@ package yougboyclub.honbabstop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // AuditingEntityListener를 사용하기 위한 어노테이션.(BaseEntity)
 public class HonbabstopApplication {
 
 	public static void main(String[] args) {
