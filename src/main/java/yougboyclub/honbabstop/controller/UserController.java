e1b6df453faf6adc0e295b58e2f224feb8c36f30
thkim610 <thkim610@naver.com>
2023-11-06 19:18:24 +0900

    mod: UserController 구현 - DB 트랙잭션 진행 #30

--- src/main/java/yougboyclub/honbabstop/controller/UserController.java
+++ src/main/java/yougboyclub/honbabstop/controller/UserController.java
@@ -1,20 +1,30 @@
 package yougboyclub.honbabstop.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yougboyclub.honbabstop.domain.User;
 import yougboyclub.honbabstop.dto.requestUserDto;
+import yougboyclub.honbabstop.service.UserService;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
 
     //회원가입
     @PostMapping("/new")
     public ResponseEntity<String> join(@RequestBody requestUserDto dto){
         System.out.println("dto = " + dto);
-        return ResponseEntity.ok().body("회원가입이 성공하였습니다.");
+
+        userService.save(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 성공하였습니다.");
     }
 }
