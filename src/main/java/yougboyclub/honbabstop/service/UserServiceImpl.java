e62bf267cf59d91a2533c8fc47f6ae29f7b2574d
thkim610 <thkim610@naver.com>
2023-11-06 19:17:29 +0900

    feat: UserService 생성 #30

---
+++ src/main/java/yougboyclub/honbabstop/service/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package yougboyclub.honbabstop.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import yougboyclub.honbabstop.domain.User;
+import yougboyclub.honbabstop.dto.requestUserDto;
+import yougboyclub.honbabstop.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Getter
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    //회원가입 (save)
+    @Override
+    public User save(requestUserDto userDto) {
+        User savedUser = userDto.toEntity(); //입력받은 정보로 User 객체 생성 후 DB에 저장.
+        System.out.println("savedUser = " + savedUser);
+        return userRepository.save(savedUser);
+    }
+}
