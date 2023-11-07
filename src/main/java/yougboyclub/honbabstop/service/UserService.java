a31c4e92568694476f3efe5bc93598467eae89e9
danbab <danbab2@naver.com>
2023-11-07 14:07:13 +0900

    mod : 회원가입 regdate 조정

--- src/main/java/yougboyclub/honbabstop/service/UserService.java
+++ src/main/java/yougboyclub/honbabstop/service/UserService.java
@@ -1,7 +1,9 @@
 package yougboyclub.honbabstop.service;
 
+import org.springframework.stereotype.Service;
 import yougboyclub.honbabstop.domain.User;
 import yougboyclub.honbabstop.dto.RequestUserDto;
+
 
 public interface UserService {
     //회원가입 (save)
