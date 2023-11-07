d5c395e5dcec0b6c175d91a40556563200eae0e4
thkim610 <thkim610@naver.com>
2023-11-07 21:00:27 +0900

    comment: EmailConfig 주석 추가 #30

--- src/main/java/yougboyclub/honbabstop/config/EmailConfig.java
+++ src/main/java/yougboyclub/honbabstop/config/EmailConfig.java
@@ -9,7 +9,9 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Configuration
 public class EmailConfig {
 
-
+    /**
+     * application.properties 파일에서 이메일 인증을 위한 세팅 값을 가져와 필드에 주입
+     */
     @Value("587")
     private int port; // SMTP 포트
 
@@ -28,7 +30,7 @@ public class EmailConfig {
     @Value("klhffwdwthcvelzz")
     private String password; // 관리자 이메일 계정 비밀번호
 
-    @Bean
+    @Bean //JavaMailSender 빈을 생성하는 메서드
     public JavaMailSender javaMailService() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
         javaMailSender.setHost("smtp.gmail.com"); // SMTP 호스트
@@ -40,6 +42,7 @@ public class EmailConfig {
         return javaMailSender;
     }
 
+    //JavaMailSender의 속성을 설정하기 위한 Properties 객체를 반환하는 메서드
     private Properties getMailProperties() {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", auth); // 인증 여부
