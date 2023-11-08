fef579d9af64eee746699d7b6c7ace4492841194
RSH <fbtnghks7906@gmail.com>
2023-11-09 02:38:17 +0900

    RequestBoardDto.java는 변경점 없음 ResponseBoardDto.java는 status 생성될때 1로 생성(글 작성되면 모집중이어서 1로 되어야함), 신청자 리스트 추가

--- src/main/java/yougboyclub/honbabstop/dto/RequestBoardDto.java
+++ src/main/java/yougboyclub/honbabstop/dto/RequestBoardDto.java
@@ -5,40 +5,36 @@ import lombok.*;
 import yougboyclub.honbabstop.domain.Board;
 import yougboyclub.honbabstop.domain.User;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
 @Builder
+@Getter
+@ToString
 public class RequestBoardDto {
-    private String restaurantName;
-    private String restaurantAddress;
-    private String foodCategory;
-    private String title;
-    private String content;
-    private User writer;
-    private Long hit;
-    private int status;
-    private int peopleLimit;
+  private String restaurantName;
+  private String restaurantAddress;
+  private User writer;
+  private String foodCategory;
+  private LocalDate time;
+  private int people;
+  private String title;
+  private String content;
+  private Long hit;
+  private int status;
 
-    public RequestBoardDto(Long id) {
-    }
-
-
-    public Board toEntity() {
-        return Board.builder()
-                .restaurantName(restaurantName)
-                .restaurantAddress(restaurantAddress)
-                .foodCategory(foodCategory)
-                .title(title)
-                .content(content)
-                .writer(writer)
-                .hit(hit)
-                .status(status)
-                .peopleLimit(peopleLimit)
-                .build();
-    };
+  public Board toEntity() {
+    return Board.builder()
+        .restaurantName(restaurantName)
+        .restaurantAddress(restaurantAddress)
+        .writer(writer)
+        .time(time)
+        .people(people)
+        .title(title)
+        .content(content)
+        .hit(hit)
+        .status(1)
+        .build();
+  }
 }
