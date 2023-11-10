8fb70397fc3abfabc00fb20cde96fef65d11fb52
codebene <114975279+rowooncoding@users.noreply.github.com>
2023-11-10 10:28:28 +0900

    Revert "Board(Entity, Service, DTO, Controller)  수정 #48 #49 #50"

--- src/main/java/yougboyclub/honbabstop/service/BoardService.java
+++ src/main/java/yougboyclub/honbabstop/service/BoardService.java
@@ -3,30 +3,20 @@ package yougboyclub.honbabstop.service;
 import yougboyclub.honbabstop.domain.Board;
 import yougboyclub.honbabstop.domain.User;
 import yougboyclub.honbabstop.dto.RequestBoardDto;
-import yougboyclub.honbabstop.dto.ResponseBoardDto;
 import yougboyclub.honbabstop.dto.UpdateBoardRequest;
 
 import java.util.List;
 
 public interface BoardService {
-  //모집글 작성
-  Board createBoard(RequestBoardDto requestBoardDto);
+    List<Board> findAllBoard();
+    List<Board> findByFoodCategory(String foodCategory);
+    List<Board> findByPlaceCategory(String placeCategory);
 
-  //모집글 전체 조회
-  List<Board> findAllBoard();
+    Board createBoard(RequestBoardDto requestBoardDto);
 
-  //모집글 상세 조회(개별 상세조회)
-  Board getBoardDetail(Long boardNo, User user);
+    // 내가 작성한 글을 조회하기 위한 메서드
+    public List<Board> findByWriter(User user);
 
-  //음식 카테고리로 게시글 조회
-  List<Board> findByFoodCategory(String foodCategory);
+    Board update(long id, UpdateBoardRequest request);
 
-  //장소 카테고리로 게시글 조회
-  List<Board> findByPlaceCategory(String placeCategory);
-
-  //내가 작성한 모든 게시글을 조회
-  List<Board> findByWriter(User user);
-
-  //모집글 수정
-  Board update(Long boardNo, UpdateBoardRequest request);
 }
