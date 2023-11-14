43b969ddb121b71a536aa40101b13ef0993cae61
rowoon <dev.rowoon@gmail.com>
2023-11-14 20:02:42 +0900

    feat: 채팅 구현 삭제  #82

--- src/main/java/yougboyclub/honbabstop/config/WebSocketConfig.java
+++ src/main/java/yougboyclub/honbabstop/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import yougboyclub.honbabstop.handler.MyWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
