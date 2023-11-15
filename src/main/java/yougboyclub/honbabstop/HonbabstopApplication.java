133375fe6d2529ad7dc0f5b87b56b7e5eaff0e5b
thkim610 <thkim610@naver.com>
2023-11-16 01:19:26 +0900

    feat: Jwt 토큰 로그인 인증을 위한 설정 구현 #91

--- src/main/java/yougboyclub/honbabstop/HonbabstopApplication.java
+++ src/main/java/yougboyclub/honbabstop/HonbabstopApplication.java
@@ -2,9 +2,10 @@ package yougboyclub.honbabstop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableJpaAuditing // AuditingEntityListener를 사용하기 위한 어노테이션.(BaseEntity - regDate 값 생성)
 public class HonbabstopApplication {
 
